a0d99fd66f22a3932dda9280c88e31d76c887159
Bill Patrianakos <bill@chooseclever.com>
2012-01-14 20:31:35 -0600

    Almost done with homepage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 /*
  * Put all your JavaScript scripts here. Plugins go elsewhere.
  */
+ 
+ // HOMEPAGE SLIDER
+ 
+$(function(){
+			$('#slides').slides({
+				preload: true,
+				generateNextPrev: true
+			});
+		});	
